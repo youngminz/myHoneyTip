02fba76e8f63d836a48f06010208484bdb6e490a
gunheekim90 <gunhee21@gmail.com>
2020-09-09 21:11:38 +0900

    2주차 실제 데이터 넣고 꾸미기 02

--- App.js
+++ App.js
@@ -14,19 +14,18 @@ export default function App() {
       <Text style={styles.title}>나만의 꿀팁</Text>
       <Image style={styles.mainImage} source={main}/>
       <ScrollView style={styles.middleContainer} horizontal indicatorStyle={"white"}>
-        <TouchableOpacity style={styles.middleButton}><Text style={styles.middleButtonText}>미용</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton}><Text style={styles.middleButtonText}>재테크</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton}><Text style={styles.middleButtonText}>할인</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton2}><Text style={styles.middleButtonText2}>꿀팁 찜</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton01}><Text style={styles.middleButtonText}>미용</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton02}><Text style={styles.middleButtonText}>재테크</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton03}><Text style={styles.middleButtonText}>할인</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton04}><Text style={styles.middleButtonText2}>꿀팁 찜</Text></TouchableOpacity>
       </ScrollView>
       <View style={styles.cardContainer}>
         {/* 하나의 카드 영역을 나타내는 View */}
-        
         <View style={styles.card}>
           <Image style={styles.cardImage} source={{uri:"https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=e698d590-98a4-4a66-b3a5-d364c8603cf1"}}/>
           <View style={styles.cardText}>
             <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게!</Text>
-            <Text style={styles.cardDesc}>먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.</Text>
+            <Text style={styles.cardDesc} numberOfLines={3}>먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.</Text>
             <Text style={styles.cardDate}>2020.09.09</Text>
           </View>
         </View>
@@ -69,34 +68,48 @@ const styles = StyleSheet.create({
     marginLeft:10,
     height:60
   },
-  middleButton: {
+  middleButton01: {
     width:100,
     height:50,
     padding:15,
-    backgroundColor:"#fff",
+    backgroundColor:"#fdc453",
     borderColor:"deeppink",
-    borderStyle:"solid",
-    borderWidth:1.5,
+    borderRadius:15,
+    margin:7
+  },
+  middleButton02: {
+    width:100,
+    height:50,
+    padding:15,
+    backgroundColor:"#fe8d6f",
+    borderRadius:15,
+    margin:7
+  },
+  middleButton03: {
+    width:100,
+    height:50,
+    padding:15,
+    backgroundColor:"#9adbc5",
     borderRadius:15,
     margin:7
   },
   middleButtonText: {
-    color:"deeppink",
+    color:"#fff",
     fontWeight:"700",
     //텍스트의 현재 위치에서의 정렬 
     textAlign:"center"
   },
-  middleButton2: {
+  middleButton04: {
     width:100,
     height:50,
     padding:15,
-    backgroundColor:"deeppink",
-    //테두리 색
-    borderColor:"deeppink",
-    //테두리 스타일 : 선은 solid, 점선은 dotted
-    borderStyle:"solid",
-    //테두리 두께
-    borderWidth:2,
+    backgroundColor:"#f886a8",
+    // //테두리 색
+    // borderColor:"deeppink",
+    // //테두리 스타일 : 선은 solid, 점선은 dotted
+    // borderStyle:"solid",
+    // //테두리 두께
+    // borderWidth:2,
     borderRadius:20,
     margin:7
   },
