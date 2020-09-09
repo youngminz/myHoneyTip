9aa1829527b0e2e20758b85609b5ba25ea442cf5
gunheekim90 <gunhee21@gmail.com>
2020-09-10 00:51:41 +0900

    2주차 앱과 자바스크립트 모듈,반복문 02

--- App.js
+++ App.js
@@ -13,17 +13,18 @@ export default function App() {
     let cardList = [];
     for(let i=0; i<tip.length; i++){
       cardList.push(<View style={styles.card}>
-        <Image style={styles.cardImage} source={{uri:"https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=e698d590-98a4-4a66-b3a5-d364c8603cf1"}}/>
+        <Image style={styles.cardImage} source={{uri:tip[i].image}}/>
         <View style={styles.cardText}>
-          <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게!</Text>
-          <Text style={styles.cardDesc} numberOfLines={3}>먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.</Text>
-          <Text style={styles.cardDate}>2020.09.09</Text>
+          <Text style={styles.cardTitle} numberOfLines={1}>{tip[i].title}</Text>
+          <Text style={styles.cardDesc} numberOfLines={3}>{tip[i].desc}</Text>
+          <Text style={styles.cardDate}>{tip[i].date}</Text>
         </View>
       </View>)
  
     }
     return cardList
   }
+
   return (
     /*
       return 구문 안에서는 {슬래시 + * 방식으로 주석
