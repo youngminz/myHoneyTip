23085df4e259af558b94e8c95e86ecc50af6b5d5
gunheekim90 <gunhee21@gmail.com>
2020-09-09 16:39:54 +0900

    Text

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import main from './assets/main.png';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
+  console.disableYellowBox = true;
+  //return 구문 밖에서는 슬래시 두개 방식으로 주석
   return (
+    /*
+      return 구문 안에서는 슬래시 + * 방식으로 주석
+    */
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>나만의 꿀팁</Text>
     </View>
   );
 }
@@ -15,7 +20,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    //
+    fontSize: 20,
+    fontWeight: '700',
+    marginTop:50,
+    marginLeft:20
   },
 });
