f8796ca8add3d01a393242ee147178c75e06ab7e
gunheekim90 <gunhee21@gmail.com>
2020-09-11 00:56:07 +0900

    3주차 디테일화면

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import MainPage from "./pages/MainPage";
-
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
 
 export default function App() {
 
   console.disableYellowBox = true;
 
-  return (<MainPage />);
+  return ( <MainPage/ > );
 }
-
