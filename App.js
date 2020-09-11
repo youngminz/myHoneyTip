ef50716d1d72a9f3b6ce3f0ebdaf8290cda1926c
gunheekim90 <gunhee21@gmail.com>
2020-09-11 15:43:57 +0900

    3주차 status bar black 적용

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import MainPage from './pages/MainPage';
 import DetailPage from './pages/DetailPage';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
 
   console.disableYellowBox = true;
 
-  return ( <MainPage/ > );
+  return ( 
+    <>
+      <StatusBar style="black" />
+      <MainPage/> 
+    </>);
 }
